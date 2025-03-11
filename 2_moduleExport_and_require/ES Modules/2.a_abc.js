c2aaffc7d4fad73728383dee9f4911c1e6583b76
23Atul <atulraman23@gmail.com>
2025-03-12 04:55:55 +0530

    multiple export

---
+++ 2_moduleExport_and_require/ES Modules/2.a_abc.js
@@ -0,0 +1,12 @@
+import { calcProduct, elem } from "../ES Modules/2.b_multiply.js"
+// const  = obj; // destructuring
+
+var a = 10;
+var b = 7;
+var c = 25;
+
+calcProduct(a,b) // 70
+
+console.log(elem*c); // 250
+
+
---
+++ 2_moduleExport_and_require/ES Modules/2.b_multiply.js
@@ -0,0 +1,12 @@
+
+export var elem = 10;
+
+export function calcProduct(a, b) {
+    const pro = a * b;
+    console.log(pro);
+}
+
+
+// either we can export each item indivisually or in form of obj
+
+// export { elem, calcProduct };   // Single export statement
